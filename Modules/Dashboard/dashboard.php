6599b88664e5f581769b37bf7c9e78b53f732934
AsSerge <z00m.serge@gmail.com>
2023-05-25 09:19:13 +0300

    Restore System6

--- Modules/Dashboard/dashboard.php
+++ Modules/Dashboard/dashboard.php
@@ -15,6 +15,22 @@
 .dataTables_filter, .dataTables_length, .pagination{
 	font-size: 0.9rem;
 }
+.pulse{
+	font-size: 0.8rem;
+}
+.pulse th, td {
+	vertical-align: middle !important;	
+}
+.pulse thead, tfoot{
+	background-color: #DADADA;
+	font-weight: 600;	
+}
+
+.superTitle{
+	display: flex;
+	justify-content: space-between;
+	flex-flow: row nowrap;	
+}
 </style>
 
 <div class="my-3 p-3 bg-white rounded box-shadow">	
@@ -42,26 +58,6 @@
 </div>
 
 
-<!--
-
-<style>
-.pulse{
-	font-size: 0.8rem;
-}
-.pulse th, td {
-	vertical-align: middle !important;	
-}
-.pulse thead, tfoot{
-	background-color: #DADADA;
-	font-weight: 600;	
-}
-
-.superTitle{
-	display: flex;
-	justify-content: space-between;
-	flex-flow: row nowrap;	
-}
-</style>
 
 <div class="my-3 p-3 bg-white rounded box-shadow">	
 	<div class='container-fluid'>
@@ -87,49 +83,48 @@
 				}else{
 					$stm = $pdo->prepare("SELECT order_id, order_key, order_type FROM orders WHERE 1");
 					$stm->execute();
-				}				
+				}
 				$orders = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-				echo "<table class='table table-sm pulse' id='ordersTable'>";
-				echo "<thead>";
-				echo "<tr><th>Заказ</th><th>Клиент</th><th>Заказ принят<br>в работу</th><th>Отправлено первое<br>предложение</th><th>Заказ одобрен<br>клиентом</th><th>Отправлен<br>на формирование</th><th>Заказ<br>общее время</th></tr>";
-				echo "</thead>";
-				echo "<tbody>";
-				foreach($orders as $ord){
+				// echo "<table class='table table-sm pulse' id='ordersTable'>";
+				// echo "<thead>";
+				// echo "<tr><th>Заказ</th><th>Клиент</th><th>Заказ принят<br>в работу</th><th>Отправлено первое<br>предложение</th><th>Заказ одобрен<br>клиентом</th><th>Отправлен<br>на формирование</th><th>Заказ<br>общее время</th></tr>";
+				// echo "</thead>";
+				// echo "<tbody>";
+				// foreach($orders as $ord){
 
-					echo "<tr>";
-					echo "<td><a href ='#' class='orderHistory'>". $ord['order_key'] . "_".$ord['order_type'] . "</a></td>";
-					echo "<td>" . $stat->getUserInfo($ord['order_id']) . "</td>";
-					echo "<td>" . $stat->getDifference($ord['order_id'],0,1,true) . "</td>";
-					echo "<td>" . $stat->getDifference($ord['order_id'],1,2,true) . "</td>";
-					echo "<td>" . $stat->getDifference($ord['order_id'],2,5,true) . "</td>";
-					echo "<td>" . $stat->getDifference($ord['order_id'],5,6,true) . "</td>";
-					echo "<td>" . $stat->getDifference($ord['order_id'],0,6,true) . "</td>";
-					echo "</tr>";
+				// 	echo "<tr>";
+				// 	echo "<td><a href ='#' class='orderHistory'>". $ord['order_key'] . "_".$ord['order_type'] . "</a></td>";
+				// 	echo "<td>" . $stat->getUserInfo($ord['order_id']) . "</td>";
+				// 	echo "<td>" . $stat->getDifference($ord['order_id'],0,1,true) . "</td>";
+				// 	echo "<td>" . $stat->getDifference($ord['order_id'],1,2,true) . "</td>";
+				// 	echo "<td>" . $stat->getDifference($ord['order_id'],2,5,true) . "</td>";
+				// 	echo "<td>" . $stat->getDifference($ord['order_id'],5,6,true) . "</td>";
+				// 	echo "<td>" . $stat->getDifference($ord['order_id'],0,6,true) . "</td>";
+				// 	echo "</tr>";
 
-					$arr1[] = $stat->getDifference($ord['order_id'],0,1,false);
-					$arr2[] = $stat->getDifference($ord['order_id'],1,2,false);
-					$arr3[] = $stat->getDifference($ord['order_id'],2,5,false);
-					$arr4[] = $stat->getDifference($ord['order_id'],5,6,false);
-					$arr5[] = $stat->getDifference($ord['order_id'],0,6,false);
-				}
-				echo "</tbody>";
-				echo "<tfoot>";
-				echo "<tr>";
-				echo "<td>Среднее время обработки</td>";
-				echo "<td></td>";
-				echo "<td>" .getAverageTime($arr1). "</td>";
-				echo "<td>" .getAverageTime($arr2). "</td>";
-				echo "<td>" .getAverageTime($arr3). "</td>";
-				echo "<td>" .getAverageTime($arr4). "</td>";
-				echo "<td>" .getAverageTime($arr5). "</td>";
-				echo "</tr>";
-				echo "</tfoot>";
-				echo "</table>";
+				// 	$arr1[] = $stat->getDifference($ord['order_id'],0,1,false);
+				// 	$arr2[] = $stat->getDifference($ord['order_id'],1,2,false);
+				// 	$arr3[] = $stat->getDifference($ord['order_id'],2,5,false);
+				// 	$arr4[] = $stat->getDifference($ord['order_id'],5,6,false);
+				// 	$arr5[] = $stat->getDifference($ord['order_id'],0,6,false);
+				// }
+				// echo "</tbody>";
+				// echo "<tfoot>";
+				// echo "<tr>";
+				// echo "<td>Среднее время обработки</td>";
+				// echo "<td></td>";
+				// echo "<td>" .getAverageTime($arr1). "</td>";
+				// echo "<td>" .getAverageTime($arr2). "</td>";
+				// echo "<td>" .getAverageTime($arr3). "</td>";
+				// echo "<td>" .getAverageTime($arr4). "</td>";
+				// echo "<td>" .getAverageTime($arr5). "</td>";
+				// echo "</tr>";
+				// echo "</tfoot>";
+				// echo "</table>";
 				?>
 			</div>
 		</div>
 	</div>
 </div>
 
--->
