dd2de5eb905976717ea99db4b717d60feb1c4ac5
AsSerge <z00m.serge@gmail.com>
2023-05-25 08:58:50 +0300

    Restore System3

--- Modules/Dashboard/dashboard.php
+++ Modules/Dashboard/dashboard.php
@@ -1,7 +1,7 @@
 <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
 			<span style="margin-right: 10px"><i class="fas fa-window-restore" style="font-size: 2.5rem;"></i></span>
 			<div class="lh-100">
-				<h6 class="mb-0 text-white lh-100">Dashboard</h6>
+				<h6 class="mb-0 text-white lh-100">Testing_Dashboard</h6>
 				<small><?php echo $user_name." " .$user_surname. " [".$user_role_description." - ".$user_team_name."]";?></small>
 			</div>
 </div>
