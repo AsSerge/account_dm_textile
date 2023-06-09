8d8848c6e369913f02ee8399f05c64c01103eaf5
AsSerge <z00m.serge@gmail.com>
2023-06-09 11:08:31 +0300

    Documents Design

--- Monitor/index.php
+++ Monitor/index.php
@@ -13,21 +13,15 @@
 <script src = "/js/jquery-3.6.0.min.js"></script>
 	<script src = "/js/popper.min.js"></script>	
 	<script src = "/js/bootstrap.min.js"></script>	
-	<script src = "/js/datatables.min.js"></script>
-	<script src = "/js/dataTables.bootstrap4.min.js"></script>
-	<script src = "/js/bootstrap-datepicker.min.js"></script>
 	<!-- Кастомизация интерфейса -->
-	<script src = "/js/custom.js"></script>	
-	<script src = '/js/jquery.form.min.js'></script>
 <script>
 
-$(document).ready(function () {
-	
-	var source = new EventSource("monitor.php?id=8");
+$(document).ready(function () {	
+	var source = new EventSource("/Monitor/monitor.php?id=8");
 	source.onmessage = function(event){
+		console.log(event.data);
 		$("#one").text("Новое число: " + event.data);
 	}
-
 });
 </script>
 </body>
